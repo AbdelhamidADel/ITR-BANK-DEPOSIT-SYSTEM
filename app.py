ce6afdc313cfc9bf345b299b3c694967e6cb1480
Abdelhamid Adel <104658866+AbdelhamidADel@users.noreply.github.com>
2023-02-03 08:16:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,33 +11,7 @@ from PIL import Image
 
 #------------------------configuration of page ----------------------------------
 im = Image.open("favicon.ico")
-st.set_page_config(layout="centered",page_icon=im,initial_sidebar_state="expanded")
-def set_page_title(title):
-    st.sidebar.markdown(unsafe_allow_html=True, body=f"""
-        <iframe height=0 srcdoc="<script>
-            const title = window.parent.document.querySelector('title') \
-                
-            const oldObserver = window.parent.titleObserver
-            if (oldObserver) {{
-                oldObserver.disconnect()
-            }} \
-
-            const newObserver = new MutationObserver(function(mutations) {{
-                const target = mutations[0].target
-                if (target.text !== '{title}') {{
-                    target.text = '{title}'
-                }}
-            }}) \
-
-            newObserver.observe(title, {{ childList: true }})
-            window.parent.titleObserver = newObserver \
-
-            title.text = '{title}'
-        </script>" />
-    """)
-
-
-set_page_title(st.text_input("ITR-Bank System"))
+st.set_page_config(layout="centered",page_icon=im,initial_sidebar_state="expanded",page_title="ITR-Bank System")
 
 #------------------------SITE BACKGROUND-------------------
 def get_base64(bin_file):
